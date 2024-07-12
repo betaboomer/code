7fd7effe68315513ee803920a1d5fc466ee2054c
betaboomer <astroboy12300@gmail.com>
2024-07-13 00:35:02 +0600

    Fixed Code

--- A.cpp
+++ A.cpp
@@ -1,6 +1,11 @@
-#ibcldesad'adsad
-'aasd
-sa
-dsa
+#include <bits/stdc++.h>
 
-'
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int x; cin >> x;
+    cout << x * 2 << '\n';
+}
